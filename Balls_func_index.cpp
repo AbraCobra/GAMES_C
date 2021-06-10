465bc6afe62c06f56f4adf021c1532018b75789f
AbraCobra <iabramova77@mail.ru>
2021-06-10 15:15:19 +0300

    new one

--- Balls_func_index.cpp
+++ Balls_func_index.cpp
@@ -2,10 +2,14 @@
 #include <cmath>
 
 void MovingObject();
-void DrawObject    (int x, int y, int vx, int vy, int a, int b, int radius);
-void PhysicsObject (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt, int* radius);
-void ControlObject (int* vx, int* vy);
-void Fon();
+void DrawObject       (float x, float y, float vx, float vy, int a, int b, int radius);
+void PhysicsObject    (float* x, float* y, float* vx, float* vy, float ax, float ay, int dt, int radius);
+void ControlObject    (float* vx, float* vy);
+void ControlCollision (float* x1, float* y1, float* x2, float* y2,
+                       float* vx1, float* vy1, float* vx2, float* vy2, int radius, int dt, int* collisions);
+float Distance        (float x1, float y1, float x2, float y2);
+void OutputScore      (int* collisions);
+
 
 int main()
     {
@@ -18,46 +22,39 @@ int main()
 
 void MovingObject()
     {
-    int x1 =   0, y1 = 0, vx1 = 2 + rand() % 10, vy1 = 2 + rand() % 10, ax1 = 1, ay1 = 0;
+    HDC  background_Cartoon = txLoadImage ("bush.bmp");
 
-    int x2 = 150, y2 = 0, vx2 = 2 + rand() % 10, vy2 = 2 + rand() % 10, ax2 = 1, ay2 = 0;
+    float x1 =   0, y1 = 0, vx1 = 5 + rand() % 10, vy1 = 5 + rand() % 10, ax1 = 1, ay1 = 0;
 
-    int x3 = 350, y3 = 0, vx3 = 2 + rand() % 10, vy3 = 2 + rand() % 10, ax3 = 1, ay3 = 0;
+    float x2 = 150, y2 = 0, vx2 = 5 + rand() % 10, vy2 = 5 + rand() % 10, ax2 = 1, ay2 = 0;
 
-    int x4 =  10, y4 = 0, vx4 = 2 + rand() % 10, vy4 = 2 + rand() % 10, ax4 = 1, ay4 = 0;
-
-    int x5 = 250, y5 = 0, vx5 = 2 + rand() % 10, vy5 = 2 + rand() % 10, ax5 = 1, ay5 = 0;
-
-    int x6 = 650, y6 = 0, vx6 = 2 + rand() % 10, vy6 = 2 + rand() % 10, ax6 = 1, ay6 = 0;
-
-    int dt = 1, radius = 35;
+    int dt = 1, radius = 55, collisions  = 0;
 
     while (!txGetAsyncKeyState (VK_ESCAPE))
         {
-        Fon();
-        txClear();
+        txBitBlt  (txDC(), 0, 0, 800, 600, background_Cartoon, 0, 0);
+
 
         DrawObject (x1, y1, vx1, vy1, 100, 200, radius);
-        DrawObject (x2, y2, vx2, vy2, 120, 175, radius);
-        DrawObject (x3, y3, vx3, vy3, 140, 150, radius);
-        DrawObject (x4, y4, vx4, vy4, 160, 125, radius);
-        DrawObject (x5, y5, vx5, vy5, 180, 100, radius);
-        DrawObject (x6, y6, vx6, vy6, 220,  75, radius);
+        DrawObject (x2, y2, vx2, vy2, 200, 175, radius);
 
-        PhysicsObject (&x1, &y1, &vx1, &vy1, ax1, ay1, dt, &radius);
-        PhysicsObject (&x2, &y2, &vx2, &vy2, ax2, ay2, dt, &radius);
-        PhysicsObject (&x3, &y3, &vx3, &vy3, ax3, ay3, dt, &radius);
-        PhysicsObject (&x4, &y4, &vx4, &vy4, ax4, ay4, dt, &radius);
-        PhysicsObject (&x5, &y5, &vx5, &vy5, ax5, ay5, dt, &radius);
-        PhysicsObject (&x6, &y6, &vx6, &vy6, ax6, ay6, dt, &radius);
+
+        PhysicsObject (&x1, &y1, &vx1, &vy1, ax1, ay1, dt, radius);
+        PhysicsObject (&x2, &y2, &vx2, &vy2, ax2, ay2, dt, radius);
+
+        ControlCollision (&x1, &y1, &x2, &y2, &vx1, &vy1, &vx2, &vy2, radius, dt, &collisions);
 
         ControlObject (&vx1, &vy1);
 
-        txSleep(40);
+        OutputScore (&collisions);
+
+        txSleep(20);
         }
+
+    txDeleteDC (background_Cartoon);
     }
 
-void DrawObject (int x, int y, int vx, int vy, int a, int b, int radius)
+void DrawObject (float x, float y, float vx, float vy, int a, int b, int radius)
     {
     for (int i = 1; i <= radius; i++)
         {
@@ -68,14 +65,13 @@ void DrawObject (int x, int y, int vx, int vy, int a, int b, int radius)
     //txLine   (x, y, x + vx*7, y + vy*7);
     }
 
-void Fon()
+float Distance (float x1, float y1, float x2, float y2)
     {
-    txSetFillColor (TX_BLACK);
-    POINT fon [4] = {{0, 0}, {800, 0}, {800, 600}, {0, 600}};
-    txPolygon (fon,4);
+    float distance = sqrt ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return distance;
     }
 
-void PhysicsObject (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt, int* radius)
+void PhysicsObject (float* x, float* y, float* vx, float* vy, float ax, float ay, int dt, int radius)
     {
    // *vx = *vx + ax * dt;
    // *vy = *vy + ay * dt;
@@ -83,36 +79,92 @@ void PhysicsObject (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt, in
     *x = *x + *vx * dt;
     *y = *y + *vy * dt;
 
-    if (*x >= 800 - *radius)
+    if (*x >= 800 - radius)
         {
         *vx = - *vx;
-        *x  = 800 - *radius;
+        *x  = 800 - radius;
         }
 
-    if (*y >= 600 - *radius)
+    if (*y >= 600 - radius)
         {
         *vy = - *vy;
-        *y  = 600 - *radius;
+        *y  = 600 - radius;
         }
 
-    if (*x <= 0 + *radius)
+    if (*x <= 0 + radius)
         {
         *vx = - *vx;
-        *x  = 0 + *radius;
+        *x  = 0 + radius;
         }
 
-    if (*y <= 0 + *radius)
+    if (*y <= 0 + radius)
         {
         *vy = - *vy;
-        *y  = 0 + *radius;
+        *y  = 0 + radius;
         }
     }
 
-void ControlObject (int* vx, int* vy)
+void ControlCollision (float* x1, float* y1, float* x2, float* y2,
+                       float* vx1, float* vy1, float* vx2, float* vy2,
+                       int radius, int dt, int* collisions)
+    {
+    float distance = Distance (*x1, *y1, *x2, *y2); if (distance == 0) distance = 0.01;
+    float angle_sin = (*x1 - *x2)/distance;
+    float angle_cos = (*y1 - *y2)/distance;
+
+      	if (distance < 2*radius)
+            {
+      		float Vn1 = *vx2*angle_sin + *vy2*angle_cos;
+
+      		float Vn2 = *vx1*angle_sin + *vy1*angle_cos;
+
+      		float dt = (2*radius - distance)/(Vn1 - Vn2);
+
+      		float Vt1 = -*vx2*angle_cos + *vy2*angle_sin;
+
+      		float Vt2 = -*vx1*angle_cos + *vy1*angle_sin;
+
+      		float temp = Vn2;
+      		Vn2 = Vn1;
+      		Vn1 = temp;
+
+      		*vx1 = Vn2*angle_sin - Vt2*angle_cos;
+
+      		*vy1 = Vn2*angle_cos + Vt2*angle_sin;
+
+      		*vx2 = Vn1*angle_sin - Vt1*angle_cos;
+
+      		*vy2 = Vn1*angle_cos + Vt1*angle_sin;
+
+      		*x1 += *vx1*dt;
+      		*y1 += *vy1*dt;
+      		*x2 += *vx2*dt;
+      		*y2 += *vy2*dt;
+
+      		*collisions += 1;
+            }
+    }
+
+void ControlObject (float* vx, float* vy)
     {
     if (txGetAsyncKeyState (VK_RIGHT)) *vx = *vx + 1;
     if (txGetAsyncKeyState (VK_LEFT))  *vx = *vx - 1;
     if (txGetAsyncKeyState (VK_UP))    *vy = *vy - 1;
     if (txGetAsyncKeyState (VK_DOWN))  *vy = *vy + 1;
     if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+    }
+
+void OutputScore(int* collisions)
+    {
+    txSetColor (TX_YELLOW);
+    txSetFillColor (TX_YELLOW);
+
+    txSelectFont ("Times", 80);
+    char strx[20] = " ";
+    sprintf (strx, "collisions =% d", *collisions);
+
+    int centerx = 220;
+    int centery = 500;
+
+    txTextOut (centerx, centery, strx);
     }
